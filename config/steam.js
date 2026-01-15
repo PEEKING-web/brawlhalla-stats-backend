df97897d1d602d83a266ca692b067b7cccb0afac
PEKKING-web <dipanshudubey61@gmail.com>
2026-01-15 21:24:49 +0530

    steam passport not holding session

--- config/steam.js
+++ config/steam.js
@@ -2,11 +2,18 @@ const passport = require('passport');
 const SteamStrategy = require('passport-steam').Strategy;
 
 passport.serializeUser((user, done) => {
-  done(null, user);
+  done(null, user.id); // or user.steamId
 });
 
-passport.deserializeUser((obj, done) => {
-  done(null, obj);
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id } // or steamId
+    });
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
 });
 
 passport.use(new SteamStrategy({
