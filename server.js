5830820b72efb18aa6c12fa1c156ab474948635e
PEKKING-web <dipanshudubey61@gmail.com>
2026-01-15 21:20:46 +0530

    domain error

--- server.js
+++ server.js
@@ -30,7 +30,8 @@ app.use(session({
     maxAge: 24 * 60 * 60 * 1000,
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    domain: 'brawlhalla-stats-backend-production.up.railway.app'
   }
 }));
 
