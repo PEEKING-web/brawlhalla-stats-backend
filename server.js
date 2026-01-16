69e5ea4e0ef5f9715109d95f8a913a2d8563ec75
PEKKING-web <dipanshudubey61@gmail.com>
2026-01-16 12:53:18 +0530

    changes to services

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(cors({
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  exposedHeaders: ['set-cookie']  // ⭐ ADD THIS
+  exposedHeaders: ['set-cookie']  // ADD THIS
 }));
 
 // Handle preflight
